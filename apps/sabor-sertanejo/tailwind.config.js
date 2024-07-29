4c5ca111903c58fb16450291c9f933e5b5cd5cf3
Matheus Eduardo <contato.nathbarbosa@gmail.com>
2024-07-29 10:27:12 -0300

    chore: add colors to tailwindcss

--- apps/sabor-sertanejo/tailwind.config.js
+++ apps/sabor-sertanejo/tailwind.config.js
@@ -5,6 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'primary': {
+        light: '#F2C292',
+        DEFAULT: '#F19C47',
+        dark: '#DF750C'
+      },
+      'secondary': {
+        light: '#2C2C2C',
+        DEFAULT: '#1E1E1E',
+        dark: '#0A0A0A'
+      },
+      'tertiary': {
+        light: '#F3F3F3',
+        DEFAULT: '#B3B3B3',
+        dark: '#767676'
+      },
+    },
     extend: {},
   },
   plugins: [],
