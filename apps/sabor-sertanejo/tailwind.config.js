4284d53683028cdb13a7ea59e6eb6dd2eafc7f35
Matheus Eduardo <contato.nathbarbosa@gmail.com>
2024-07-29 11:40:13 -0300

    fix: change tailwind.config.js to not override the default colors

--- apps/sabor-sertanejo/tailwind.config.js
+++ apps/sabor-sertanejo/tailwind.config.js
@@ -1,27 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      primary: {
-        light: "#F2C292",
-        DEFAULT: "#F19C47",
-        dark: "#DF750C",
-      },
-      secondary: {
-        light: "#2C2C2C",
-        DEFAULT: "#1E1E1E",
-        dark: "#0A0A0A",
-      },
-      tertiary: {
-        light: "#F3F3F3",
-        DEFAULT: "#B3B3B3",
-        dark: "#767676",
-      },
-    },
     extend: {
       fontFamily: {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: {
+          light: "#F2C292",
+          DEFAULT: "#F19C47",
+          dark: "#DF750C",
+        },
+        secondary: {
+          light: "#2C2C2C",
+          DEFAULT: "#1E1E1E",
+          dark: "#0A0A0A",
+        },
+        tertiary: {
+          light: "#F3F3F3",
+          DEFAULT: "#B3B3B3",
+          dark: "#767676",
+        },
       },
     },
   },
