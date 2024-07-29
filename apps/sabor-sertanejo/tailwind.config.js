a346dcfa21870ab5b753d325597b8c60970470bf
Matheus Eduardo <contato.nathbarbosa@gmail.com>
2024-07-29 10:41:53 -0300

    style: format tailwind.config.js

--- apps/sabor-sertanejo/tailwind.config.js
+++ apps/sabor-sertanejo/tailwind.config.js
@@ -1,25 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
-      'primary': {
-        light: '#F2C292',
-        DEFAULT: '#F19C47',
-        dark: '#DF750C'
+      primary: {
+        light: "#F2C292",
+        DEFAULT: "#F19C47",
+        dark: "#DF750C",
       },
-      'secondary': {
-        light: '#2C2C2C',
-        DEFAULT: '#1E1E1E',
-        dark: '#0A0A0A'
+      secondary: {
+        light: "#2C2C2C",
+        DEFAULT: "#1E1E1E",
+        dark: "#0A0A0A",
       },
-      'tertiary': {
-        light: '#F3F3F3',
-        DEFAULT: '#B3B3B3',
-        dark: '#767676'
+      tertiary: {
+        light: "#F3F3F3",
+        DEFAULT: "#B3B3B3",
+        dark: "#767676",
       },
     },
     extend: {
@@ -29,5 +26,4 @@ export default {
     },
   },
   plugins: [],
-}
-
+};
