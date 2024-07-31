7e3fb8a8658297782208685746b63cf169d974ad
vitor menezes <vm1522279@gmail.com>
2024-07-31 15:42:09 -0300

    feat: add Input

---
+++ apps/sabor-sertanejo/src/components/Input/index.tsx
@@ -0,0 +1,9 @@
+import InputRoot from "./InputRoot";
+import InputLabel from "./InputLabel";
+import InputContent from "./InputContent";
+
+export const Input = {
+  Root: InputRoot,
+  Label: InputLabel,
+  Content: InputContent,
+};
