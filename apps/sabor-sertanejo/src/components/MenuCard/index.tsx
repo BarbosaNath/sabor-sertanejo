80e8620587d30c1755017353ed671c70543e6edb
Matheus Eduardo <contato.nathbarbosa@gmail.com>
2024-08-01 12:21:01 -0300

    feat: add MenuCard

---
+++ apps/sabor-sertanejo/src/components/MenuCard/index.tsx
@@ -0,0 +1,9 @@
+import MenuCardIcon from "./MenuCardIcon";
+import MenuCardLabel from "./MenuCardLabel";
+import MenuCardRoot from "./MenuCardRoot";
+
+export const MenuCard = {
+  Icon: MenuCardIcon,
+  Label: MenuCardLabel,
+  Root: MenuCardRoot,
+};
