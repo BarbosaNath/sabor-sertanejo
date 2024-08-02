e1b99f2eb6c700bf169125cd04d4c58e353d3bd9
Matheus Eduardo <contato.nathbarbosa@gmail.com>
2024-08-02 16:39:13 -0300

    chore: add class-validator class-transformer

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,12 +3,14 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PizzaModule } from './pizza/pizza.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '../..', 'sabor-sertanejo', "dist"),
-    })
+      rootPath: join(__dirname, '../..', 'sabor-sertanejo', 'dist'),
+    }),
+    PizzaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
