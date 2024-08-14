d66d98057d4287878b82ccdecd1c7c373edfd20c
Matheus Eduardo <contato.nathbarbosa@gmail.com>
2024-08-14 14:37:09 -0300

    feat: made backdrop top and bottom optional

--- apps/sabor-sertanejo/src/components/Backdrop/index.tsx
+++ apps/sabor-sertanejo/src/components/Backdrop/index.tsx
@@ -5,25 +5,36 @@ import BackdropBottom from "../../assets/backdrop_b.png";
 interface BackdropProps {
   children: ReactNode;
   dark?: boolean;
+  top?: boolean;
+  bottom?: boolean;
 }
 
-function Backdrop({ children, dark }: BackdropProps) {
+function Backdrop({
+  children,
+  dark,
+  top = true,
+  bottom = true,
+}: BackdropProps) {
   return (
     <div className="flex flex-col gap-0 m-0 p-0">
-      <div
-        style={{ backgroundImage: `url(${BackdropTop})` }}
-        className="w-full h-10"
-      />
+      {top && (
+        <div
+          style={{ backgroundImage: `url(${BackdropTop})` }}
+          className="w-full h-10"
+        />
+      )}
       <div
         data-dark={dark}
         className="flex flex-col justify-center items-center bg-primary data-[dark=true]:bg-secondary-light py-8"
       >
         {children}
       </div>
-      <div
-        style={{ backgroundImage: `url(${BackdropBottom})` }}
-        className="w-full h-10"
-      />
+      {bottom && (
+        <div
+          style={{ backgroundImage: `url(${BackdropBottom})` }}
+          className="w-full h-10"
+        />
+      )}
     </div>
   );
 }
