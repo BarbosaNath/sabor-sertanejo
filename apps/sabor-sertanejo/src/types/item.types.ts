03cc12b7fb7bfb29868cb5caf0f99647d03a7b95
Matheus Eduardo <contato.nathbarbosa@gmail.com>
2024-08-20 15:57:23 -0300

    feat: add item type

---
+++ apps/sabor-sertanejo/src/types/item.types.ts
@@ -0,0 +1,13 @@
+export interface ItemType {
+  id: string;
+  name: string;
+  imageURL: string;
+  price: number;
+  originalPrice: number;
+  description: string;
+  stock: number;
+  category: string;
+  estimatedTime: number;
+  createdAt: string;
+  updatedAt: string;
+}
