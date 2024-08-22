264c0ba01438dd60bb51c8e528e5bbc47fe93c6f
Matheus Eduardo <contato.nathbarbosa@gmail.com>
2024-08-22 14:09:48 -0300

    feat: refactor menu card component

--- apps/sabor-sertanejo/src/components/MenuCard/index.tsx
+++ apps/sabor-sertanejo/src/components/MenuCard/index.tsx
@@ -1,9 +1,40 @@
-import MenuCardIcon from "./MenuCardIcon";
-import MenuCardLabel from "./MenuCardLabel";
-import MenuCardRoot from "./MenuCardRoot";
+import { ButtonHTMLAttributes, ElementType } from "react";
+import menuCardBackground from "./MenuCardBackground.svg";
+import menuCardBackgroundLarge from "./MenuCardBackgroundLarge.svg";
+import { tv } from "tailwind-variants";
 
-export const MenuCard = {
-  Icon: MenuCardIcon,
-  Label: MenuCardLabel,
-  Root: MenuCardRoot,
-};
+interface MenuCardRootProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  label: string;
+  icon: ElementType;
+  large?: boolean;
+}
+
+const menuCard = tv({
+  base: "relative flex flex-col justify-center items-center gap-5 bg-white hover:bg-tertiary-light bg-no-repeat drop-shadow-md rounded-lg w-[100px] h-[100px] text-xl text-secondary",
+
+  variants: {
+    large: {
+      true: "w-[210px]",
+    },
+  },
+});
+
+function MenuCard({ icon: Icon, label, large, ...rest }: MenuCardRootProps) {
+  return (
+    <button
+      {...rest}
+      className={menuCard({ large, className: rest.className })}
+      style={{
+        backgroundImage: large
+          ? `url(${menuCardBackgroundLarge})`
+          : `url(${menuCardBackground})`,
+      }}
+    >
+      <Icon />
+
+      <p>{label}</p>
+    </button>
+  );
+}
+
+export default MenuCard;
